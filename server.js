691690337a01eecfc9b71dd8dfdf3737e88d035c
Guilherme Borges Arimatea <aeis.gui@hotmail.com>
2025-04-09 17:53:12 -0300

    Class2 added routes and nodemon

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const http = require('http');
 const port = 3000;
 
+const rotas = {
+    '/': 'Curso de Node.js',
+    '/livros': 'Entrei na página de livros',
+    '/autores': 'Entrei na página de autores',
+    '/editora': 'Entrei na página de editoras',
+    '/sobre': 'Entrei na página sobre',
+}
+
 const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
 
-    res.end('Hello, World!\n');
+    res.end(rotas[req.url] || 'Página não encontrada');
 });
 
 server.listen(port, () => {
